b51fd61bf10c1c0bcc0a49e34a21fcabef13126a
nicolau <marcosnicolau2003@gmail.com>
2023-09-29 15:42:45 -0300

    script fachero
    se crea un script fachero para correr los ejercicios sin el hassle de compilar

--- tps/02-Automatas/run.sh
+++ tps/02-Automatas/run.sh
@@ -1,2 +1,16 @@
-gcc ./ejercicio_2.c  -o ./app.out
-./app.out $1
+show_help(){
+    echo "Usage: $ ./run.sh <PROGRAM_NUMBER> <ARGUMENT>\n"
+    echo "EXAMPLE:"
+    echo "Run the first program with the string 012\$90\$0x321:"
+    echo "$ ./run.sh 1 012\\\$90\\\$0x321\n"
+    echo "Or run the second program and solve 3*4+12/6:"
+    echo "$ ./run.sh 2 3*4+12/6"
+}
+
+if [ -z $1 ] || [ $1 = "--help" ] || [ $1 = "-h" ]; then 
+    show_help
+    exit 0
+fi
+
+make $1 -s
+./app.out $2 
